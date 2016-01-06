48c4b5886c3979ccf39f3d0d2364c7963d2ca894
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-01-06 17:09:10 +0100

    setting

--- index.php
+++ index.php
@@ -1,8 +1,35 @@
 <?php 
 
+use Phalcon\Loader;
 use Phalcon\Mvc\Micro;
+use Phalcon\DI\FactoryDefault;
+use Phalcon\Db\Adapter\Pdo\Mysql as PdoMysql;
 
-$app = new Micro();
+// use Loader() to autoload our model
+$loader = new Loader();
+
+$loader->registerDirs(
+    array(
+        __DIR__ . '/models/'
+    )
+)->register();
+
+$di = new FactoryDefault();
+
+// Set up the database service
+$di->set('db', function() {
+    return new PdoMysql(
+        array(
+            "host"      => "localhost",
+            "username"  => "root",
+            "password"  => "0000",
+            "dbname"    => "phalcapi"
+        )
+    );
+});
+
+// Create and bind the DI to the application
+$app = new Micro($di);
 
 // Define the routes
 
