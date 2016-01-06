27394ab54b56a56187a1be090d2bd4a90e34f42e
Nayed Saïd Ali <nayed.saidali@gmail.com>
2016-01-06 17:14:01 +0100

    all robot

--- index.php
+++ index.php
@@ -34,8 +34,21 @@ $app = new Micro($di);
 // Define the routes
 
 // Retrieves all robots
-$app->get('/api/robots', function() {
+$app->get('/api/robots', function() use ($app) {
 
+    $phql = "SELECT * FROM Robots ORDER BY name";
+    $robots = $app->modelsManager->executeQuery($phql);
+
+    $data = array();
+
+    foreach ($robots as $robot) {
+        $data[] = array(
+            'id'    => $robot->id,
+            'name'  => $robot->name
+        );
+    }
+
+    echo json_encode($data);
 });
 
 // Searches for robots with $name in their name
